5ae9dc037a386c91abd33eb4205cfdf580263cce
Gajjamal <87606589+signife@users.noreply.github.com>
2023-04-05 17:20:28 +0900

    5.20

---
+++ StepByStep/6-BaekjoonIntensify/25206너의 평점은/25206 너의 평점은.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    for (int i = 0; i < 20; i++)
+    {
+        cin >> s;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == ' ')
+                A +
+        }
+    }
+}
