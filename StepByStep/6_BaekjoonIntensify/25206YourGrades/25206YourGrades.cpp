2f40f710a50c9f0650e33c7e636378ecb2538680
Gajjamal <87606589+signife@users.noreply.github.com>
2023-04-11 14:51:25 +0900

    bumgrui

--- StepByStep/6_BaekjoonIntensify/25206YourGrades/25206YourGrades.cpp
+++ StepByStep/6_BaekjoonIntensify/25206YourGrades/25206YourGrades.cpp
@@ -4,11 +4,77 @@ using namespace std;
 
 int main()
 {
-    string s;
+    string subject, grade;
+    int score, total_score;
+
     for (int i = 0; i < 20; i++)
     {
-        cin >> s;
-        cout << s;
+        cin >> subject >> score >> grade;
+        switch (grade[0])
+        {
+        }
     }
+
+    double avg_score = total_score / total_credit;
+
+    if (avg_score >= 3.3 || total_credit >= 140)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define FASTIO   \
+//     cin.tie(0);  \
+//     cout.tie(0); \
+//     ios_base::sync_with_stdio(0);
+// #define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+// #define ROF(i, a, b) for (int i = (a); i >= (b); i--)
+// #define ll long long int
+// #define pii pair<int, int>
+// #define PQ priority_queue
+// #define LEN(v) int(v.size())
+// #define ALL(v) v.begin(), v.end()
+// #define INF int(2e9)
+// #define LINF ll(1e18)
+
+// map<string, double> mp;
+
+// int main()
+// {
+//     FASTIO;
+//     mp.insert({"A+", 4.5});
+//     mp.insert({"A0", 4.0});
+//     mp.insert({"B+", 3.5});
+//     mp.insert({"B0", 3.0});
+//     mp.insert({"C+", 2.5});
+//     mp.insert({"C0", 2.0});
+//     mp.insert({"D+", 1.5});
+//     mp.insert({"D0", 1.0});
+//     mp.insert({"F", 0.0});
+
+//     double result = 0;
+//     double score_sum = 0;
+
+//     FOR(i, 1, 20)
+//     {
+//         string name;
+//         double score;
+//         string grade;
+//         cin >> name >> score >> grade;
+//         if (grade == "P")
+//             continue;
+//         result += score * mp[grade];
+//         score_sum += score;
+//     }
+//     result /= score_sum;
+
+//     cout << fixed << setprecision(4);
+//     cout << result;
+
+//     return 0;
+// }
