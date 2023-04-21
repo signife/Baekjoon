61c2a9d8b65adc3b0d926732765b3fc98d71828f
Gajjamal <87606589+signife@users.noreply.github.com>
2023-04-21 17:34:15 +0900

    add: 어려워서 참조한 소스

---
+++ BasicAlgoLecture/2Basic/1267.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int phone[10000], Y, M;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    for (int n = 0; n < N; n++)
+        cin >> phone[n];
+
+    for (int n = 0; n < N; n++)
+        Y += ((phone[n] / 30) + 1) * 10;
+
+    for (int n = 0; n < N; n++)
+        M += ((phone[n] / 60) + 1) * 15;
+
+    if (Y < M)
+        cout << "Y " << Y;
+    else if (Y > M)
+        cout << "M " << M;
+    else
+        cout << "Y M " << Y;
+}
