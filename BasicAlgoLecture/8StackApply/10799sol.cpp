5314b0e46bedcbc8c74bda908cb11c7479e8b388
Gajjamal <87606589+signife@users.noreply.github.com>
2023-06-30 17:30:44 +0900

    add: new source code

--- BasicAlgoLecture/8StackApply/10799sol.cpp
+++ BasicAlgoLecture/8StackApply/10799sol.cpp
@@ -33,38 +33,3 @@ int main(void)
     cout << count;
     return 0;
 }
-
-// #include <iostream>
-// #include <string>
-// #include <stack>
-
-// using namespace std;
-
-// int main()
-// {
-//     ios::sync_with_stdio(false);
-//     cin.tie(0);
-
-//     stack<char> S;
-//     string input;
-//     int total = 0;
-//     cin >> input;
-//     for (int i = 0; i < input.length(); i++)
-//     {
-//         if (input[i] == '(')
-//         {
-//             S.push(input[i]);
-//         }
-//         else if (input[i] == ')' and input[i - 1] == '(')
-//         { // 레이저일때
-//             S.pop();
-//             total += S.size();
-//         }
-//         else
-//         { // 마지막 자투리일때
-//             total++;
-//             S.pop();
-//         }
-//     }
-//     cout << total << endl;
-// }
